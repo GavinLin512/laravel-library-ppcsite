a987b1378ab25b053afdd1126edf11b81d65e114
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2023-03-29 17:09:56 +0800

    reverse

--- packages/ppcsite/view_component/resources/views/components/form_input_col6.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="form-group col-6 d-flex flex-column flex-md-row">
-    <label class="col-form-label col-md-3" for="{{ $field??'' }}"> {{ $label_name??'' }} </label>
-    <input id="{{ $field??'' }}" class="form-control" type="{{ $type??'text' }}" placeholder="{{ $placeholder??'' }}" name="{{ $field??'' }}"/>
-</div>
--- packages/ppcsite/view_component/src/ViewComponent.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Ppcsite\ViewComponent;
-
-Class ViewComponent
-{
-
-}
