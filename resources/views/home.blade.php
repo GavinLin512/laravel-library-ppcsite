7e1f97ac354d842dc0443fc5d5843fb40435234f
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2023-04-18 16:59:54 +0800

    edit component form input

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,15 @@
     <x-Component::card_custom color="cyan">
         <x-slot:header>填寫資料</x-slot:header>
         <x-slot:content>
-
+            <div class="row">
+                @foreach(['姓名','通訊地址2324324'] as $label_name)
+                    <x-Component::form_input_custom col="12" field="name" type="file" prepend="true" append="true" required="true">
+                        <x-slot:label_name>{{ $label_name }}</x-slot:label_name>
+                        <x-slot:prepend_text>text</x-slot:prepend_text>
+                        <x-slot:append_text><input type="checkbox"/></x-slot:append_text>
+                    </x-Component::form_input_custom>
+                @endforeach
+            </div>
         </x-slot:content>
     </x-Component::card_custom>
 
