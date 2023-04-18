bb59ef57d9c7cea116eef530d05146a8c25cacea
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2023-04-18 16:58:46 +0800

    add component card (init)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,25 +8,21 @@
 
 @section('content')
     <p>Welcome to this beautiful admin panel.</p>
-    <div class="card card-cyan">
-        <div class="card-header">測試卡片</div>
-        <div class="card-body">
-            <div class="row">
-                @foreach(['姓名','通訊地址'] as $label_name)
-                    <x-Component::form_input_col6 field="name">
-                        <x-slot:label_name>{{ $label_name }}</x-slot:label_name>
-                    </x-Component::form_input_col6>
-                @endforeach
-            </div>
-        </div>
-    </div>
+    <x-Component::card_custom color="cyan">
+        <x-slot:header>填寫資料</x-slot:header>
+        <x-slot:content>
+
+        </x-slot:content>
+    </x-Component::card_custom>
 
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    <script>
+        console.log('Hi!');
+    </script>
 @stop
